5f29eb4f435a2dac25ea51344bdb597a247b4fc4
joe <joe.guo@oocl.com>
2019-09-11 16:48:51 +0800

    Chapter 6

---
+++ swift-ios-10-book/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Bale Lin on 2019/9/10.
+//  Copyright © 2019年 Joe Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
