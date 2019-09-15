6bc951941cb3cb48504ecd92c5655715e63149c2
sicmatr1x <sicmatr1x@outlook.com>
2019-09-15 22:29:28 +0800

    add chapter 8

---
+++ swift-ios-10-book/FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by sicmatr1x on 2019/9/15.
+//  Copyright © 2019 Joe Guo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
